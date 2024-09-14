b6bad16c0e990498ca31a893735907427062e3d4
JAGNYASENI NAIK <reenunaik711@gmail.com>
2024-09-14 23:03:01 +0530

    added

--- JAVASCRIPT/chapter 1 variables/02_index.js
+++ JAVASCRIPT/chapter 1 variables/02_index.js
@@ -4,4 +4,4 @@ console.log("a");
  a="reenu"
  console.log(a)
  //let 9reenu= 4 //not allowed
- // let var=9 this also not allowed
+ // let var=9 this also not allowed // case sensitive
---
+++ JAVASCRIPT/chapter 1 variables/03_var_let_const.js
@@ -0,0 +1,17 @@
+console.log("my name is jagnyaseni")
+var a =78;
+var b = "jagnyaseni";
+let g = "jagn";
+var c ="null"
+var d = "undefined"
+
+{
+    var b ="youu"
+    console.log(b)
+} 
+console.log(b)
+{
+    let g ="bench"
+    console.log(g)
+} 
+console.log(g)
