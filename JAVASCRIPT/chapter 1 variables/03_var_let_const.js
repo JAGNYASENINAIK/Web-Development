12578cb593f23df842239281841cc98c2c708ba8
JAGNYASENI NAIK <reenunaik711@gmail.com>
2024-09-19 16:54:54 +0530

    added

--- JAVASCRIPT/chapter 1 variables/03_var_let_const.js
+++ JAVASCRIPT/chapter 1 variables/03_var_let_const.js
@@ -4,18 +4,20 @@ var b = "jagnyaseni";
 let g = "jagn";
 var c ="null"
 var d = "undefined"
-
+// if we use let in a block and change the value it will bw changed everywhere
 {
     var b ="youu"
     console.log(b)
 } 
 console.log(b)
+//if we use let in block only block will change
 {
     let g ="bench"
     console.log(g)
 } 
 console.log(g)
-
+ //if once const is declared it can not be changed//
 const author =  "reenu"
+//const author="menu"
 // let author="9" throws error
 console.log(author)
