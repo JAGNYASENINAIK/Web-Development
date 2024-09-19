b168e09dcc2826381e0f31a99551830adc42e86f
JAGNYASENI NAIK <reenunaik711@gmail.com>
2024-09-19 17:12:47 +0530

    added

---
+++ JAVASCRIPT/chapter 1 variables/04_primitives_&_objects.js
@@ -0,0 +1,16 @@
+//N=NULL
+//N=NUMBER
+//B=BIGINT
+//B=BOOLEAN
+//S=STRING
+//S=SYMBOL
+//U=UNDEFINED
+
+let a =null;
+let b =345;
+let c =true // can also  be false
+let d =BigInt("67879") + BigInt("1");
+let e ="REENU"
+let f =Symbol("I am a nice symbol")
+let g =undefined
+console.log(a,b,c,d,e,f,g)
