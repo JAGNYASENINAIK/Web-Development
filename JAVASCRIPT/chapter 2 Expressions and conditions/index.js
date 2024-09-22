47066acaced7d3faabeaadaaa08f297846127e5d
JAGNYASENI NAIK <reenunaik711@gmail.com>
2024-09-22 16:03:37 +0530

    added

---
+++ JAVASCRIPT/chapter 2 Expressions and conditions/index.js
@@ -0,0 +1,20 @@
+//77;
+//"reenu"
+//true
+console.log("operators in js")
+let a =10;
+let b =4;
+console.log("a+b=",(a+b));
+console.log("a-b=",(a-b));
+console.log("a/b=",(a/b));
+console.log("a*b=",(a*b));
+console.log("a**b=",(a**b));
+console.log("a%b=",(a%b));
+console.log("a++",(a++)); //first it will print and then increase
+console.log("a=",a)
+console.log("++a",(++a)); //it will increase and print
+console.log("a=",a)
+console.log("a--",(a--)); //it will print then decrease
+console.log("a=",a)
+console.log("--a",(--a)); //decrease then print
+console.log("a=",a)
