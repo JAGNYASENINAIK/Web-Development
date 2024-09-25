678bfa6f920037db572f0e5d27d4bb51e1df9348
JAGNYASENI NAIK <reenunaik711@gmail.com>
2024-09-25 22:35:35 +0530

    added

--- JAVASCRIPT/chapter 2 Expressions and conditions/conditional_exp.js
+++ JAVASCRIPT/chapter 2 Expressions and conditions/conditional_exp.js
@@ -5,5 +5,10 @@ alert("This is an invalid age")
 }
 else if(a<9){
     alert("you are a kid and you cannot even think of driving")
-
 }
+else if(a<18 && a>=9){
+    alert("you are a kid and you can think of driving after 18")
+}
+// else{
+//     alert("you can now drive")
+// }
