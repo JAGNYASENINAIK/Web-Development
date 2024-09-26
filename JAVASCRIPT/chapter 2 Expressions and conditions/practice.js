b0ce3e1b2698a258032feaa29f90c69a26e57570
JAGNYASENI NAIK <reenunaik711@gmail.com>
2024-09-26 22:53:48 +0530

    added

--- JAVASCRIPT/chapter 2 Expressions and conditions/practice.js
+++ JAVASCRIPT/chapter 2 Expressions and conditions/practice.js
@@ -15,4 +15,12 @@ let age = prompt("what is your age")
 switch(age){
     case 12:
         console.log("your age is 12")
+    case 13:
+        console.log("your age is 13")
+    case 14:
+        console.log("your age is 14")
+    case 15:
+        console.log("your age is 15")
+    case 16:
+        console.log("your age is 16")
 }
