0f192e6db82b16451e1ebd815281e738e3575814
JAGNYASENI NAIK <reenunaik711@gmail.com>
2024-09-26 22:44:20 +0530

    added

---
+++ JAVASCRIPT/chapter 2 Expressions and conditions/practice.js
@@ -0,0 +1,8 @@
+const prompt = require("prompt-sync")({ sigint: true });
+let age = prompt("what is your age")
+if(age>10 && age<20){
+    console.log("your age lies between 10 & 20")
+}
+else{
+    console.log("your age  does not lies between 10 & 20")
+}
