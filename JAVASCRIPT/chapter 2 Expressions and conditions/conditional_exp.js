c061330baf7d9cdfa2b5c80157d1f1ec54e4da47
JAGNYASENI NAIK <reenunaik711@gmail.com>
2024-09-26 22:26:06 +0530

    added

--- JAVASCRIPT/chapter 2 Expressions and conditions/conditional_exp.js
+++ JAVASCRIPT/chapter 2 Expressions and conditions/conditional_exp.js
@@ -17,36 +17,38 @@
 // }
 
 //chtg
-// const prompt = require("prompt-sync")({ sigint: true });
+const prompt = require("prompt-sync")({ sigint: true });
 
-// let a = prompt("Hey, what's your age? ");
-// a = Number.parseInt(a); // converts string to number
+let a = prompt("Hey, what's your age? ");
+a = Number.parseInt(a); // converts string to number
 
-// if (isNaN(a)) {
-//     console.log("This is an invalid age");
-// } else if (a < 0) {
-//     console.log("This is an invalid age");
-// } else if (a < 9) {
-//     console.log("You are a kid and you cannot even think of driving");
-// } else if (a < 18 && a >= 9) {
-//     console.log("You are a kid and you can think of driving after 18");
-// } else {
-//     console.log("You can now drive as you are above 18");
-// }
-// console.log("Done")
+if (isNaN(a)) {
+    console.log("This is an invalid age");
+} else if (a < 0) {
+    console.log("This is an invalid age");
+} else if (a < 9) {
+    console.log("You are a kid and you cannot even think of driving");
+} else if (a < 18 && a >= 9) {
+    console.log("You are a kid and you can think of driving after 18");
+} else {
+    console.log("You can now drive as you are above 18");
+}
+console.log("Done");
+console.log("you can", (a<18? "not drive":"drive"));
+
 
 
 // switch statement
-const expr = 'Papayas';
-switch (expr) {
-  case 'Oranges':
-    console.log('Oranges are $0.59 a pound.');
-    break;
-  case 'Mangoes':
-  case 'Papayas':
-    console.log('Mangoes and papayas are $2.79 a pound.');
-    // Expected output: "Mangoes and papayas are $2.79 a pound."
-    break;
-  default:
-    console.log(`Sorry, we are out of ${expr}.`);
-}
+// const expr = 'Papayas';
+// switch (expr) {
+//   case 'Oranges':
+//     console.log('Oranges are $0.59 a pound.');
+//     break;
+//   case 'Mangoes':
+//   case 'Papayas':
+//     console.log('Mangoes and papayas are $2.79 a pound.');
+//     // Expected output: "Mangoes and papayas are $2.79 a pound."
+//     break;
+//   default:
+//     console.log(`Sorry, we are out of ${expr}.`);
+// }
