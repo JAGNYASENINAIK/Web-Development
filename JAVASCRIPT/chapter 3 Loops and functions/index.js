b5836deba542b3184682d2ee87b641caedd7cc20
JAGNYASENI NAIK <reenunaik711@gmail.com>
2024-09-27 22:25:50 +0530

    added

--- JAVASCRIPT/chapter 3 Loops and functions/index.js
+++ JAVASCRIPT/chapter 3 Loops and functions/index.js
@@ -1,3 +1,22 @@
-for(let i =0 ;i<25;i++){
-    console.log(i);
+// //program to add first n natural numbers
+// let sum=0
+// const prompt=require("prompt-sync")({sigint:true}); 
+// let n = prompt("enter the value of n :")
+// n=Number.parseInt(n)
+// for(let i =0 ;i<n;i++){
+//     sum+=(i+1) // sum=sum+(i+1)
+//     console.log(i+1) ,"+";
+// }
+// console.log("sum of first "   +n   + "-natural number is "  +    sum) 
+
+//factorial
+
+let fact=1
+const prompt=require("prompt-sync")({sigint:true}); 
+let n = prompt("enter the value of n :")
+n=Number.parseInt(n)
+for(let i =1 ;i<n;i++){
+    fact*=(i+1) 
+    //console.log(i*1) ,"+";
 }
+console.log("factorial of first "   +n   + "-natural number is "  +    fact) 
