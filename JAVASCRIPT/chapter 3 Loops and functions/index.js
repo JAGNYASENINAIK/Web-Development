57f59310f0aa438de59028bdeb2a54841d2cf946
JAGNYASENI NAIK <reenunaik711@gmail.com>
2024-09-28 19:32:16 +0530

    updated index.js

--- JAVASCRIPT/chapter 3 Loops and functions/index.js
+++ JAVASCRIPT/chapter 3 Loops and functions/index.js
@@ -9,6 +9,18 @@
 // }
 // console.log("sum of first "   +n   + "-natural number is "  +    sum) 
 
+let sum=0
+const prompt=require("prompt-sync")({sigint:true}); 
+let n = prompt("enter the value of n :")
+n=Number.parseInt(n)
+for(var i =0 ;i<n;i++){
+    sum+=(i+1) // sum=sum+(i+1)
+    console.log(i+1) ,"+";
+}
+console.log("sum of first "   +n   + "-natural number is "  +    sum) 
+
+console.log(i)
+
 // //factorial
 
 // let fact=1
@@ -21,13 +33,17 @@
 // }
 // console.log("factorial of first "   +n   + "-natural number is "  +    fact) 
 
-let obj ={
-    reenu:90,
-    sinu:89,
-    rahul:78,
-    jakul:60,
-    pritul:20,
-}
-for ( let a in obj){
-    console.log("Marks of  " +a+ " are" + - obj[a])
-}
+// let obj ={
+//     reenu:90,
+//     sinu:89,
+//     rahul:78,
+//     jakul:60,
+//     pritul:20,
+// }
+// for ( let a in obj){
+//     console.log("Marks of  " +a+ " are" + - obj[a])
+// }
+
+// for (let b of "reenu"){
+//     console.log(b)
+// }
