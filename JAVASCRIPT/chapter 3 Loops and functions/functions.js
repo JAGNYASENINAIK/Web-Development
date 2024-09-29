98bc614f136ffc5bcf41a82b2a315393b1cbc577
JAGNYASENI NAIK <reenunaik711@gmail.com>
2024-09-29 11:47:30 +0530

    added

--- JAVASCRIPT/chapter 3 Loops and functions/functions.js
+++ JAVASCRIPT/chapter 3 Loops and functions/functions.js
@@ -1,9 +1,15 @@
 function onePlusAvg(x,y){
+    console.log("done")
     return 1+(x+y)/2
 }
+const sum = (p,q)=>{
+    return p+q
+}
+
 let a = 3
 let b = 7
 let c = 5
 console.log("one plus average of a and b is",onePlusAvg(a,b))
 console.log("one plus average of b and c is",onePlusAvg(b,c))
 console.log("one plus average of a and c is",onePlusAvg(a,c))
+console.log(sum(9,7))
--- JAVASCRIPT/chapter 3 Loops and functions/tempCodeRunnerFile.js
+++ JAVASCRIPT/chapter 3 Loops and functions/tempCodeRunnerFile.js
@@ -1,5 +1,6 @@
-let count = 1;
-// while (count <= 5) {
-//   console.log(count);
-//   count++;
-// }
+let a = 3
+// let b = 7
+// let c = 5
+// console.log("one plus average of a and b is",onePlusAvg(a,b))
+// console.log("one plus average of b and c is",onePlusAvg(b,c))
+// console.log("one plu
