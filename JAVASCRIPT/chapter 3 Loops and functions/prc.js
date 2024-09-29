4e376dd1fb20d209c0dd1385f59a1aead37ce80b
JAGNYASENI NAIK <reenunaik711@gmail.com>
2024-09-29 21:24:11 +0530

    updated

--- JAVASCRIPT/chapter 3 Loops and functions/prc.js
+++ JAVASCRIPT/chapter 3 Loops and functions/prc.js
@@ -9,20 +9,20 @@
 //     console.log("marks of " +Object.keys(marks)[i]+  "are  :"  + ( marks[Object.keys(marks)[i]]))
 //     }
 
-// pro-2
-for(let i in marks){
-    console.log("marks of " +i+ " are :"  + marks[i])
-}
-
-//pro-3
-// let cn = 498;
-// const prompt=require("prompt-sync")({sigint:true}); 
-// let i 
-// while(i!=cn){
-//     console.log("Try Again")
-//     i=prompt("Enter a number : ")
+// // pro-2
+// for(let i in marks){
+//     console.log("marks of " +i+ " are :"  + marks[i])
 // }
-// console.log("you have entered a correct number")
+
+pro-3
+let cn = 498;
+const prompt=require("prompt-sync")({sigint:true}); 
+let i 
+while(i!=cn){
+    console.log("Try Again")
+    i=prompt("Enter a number : ")
+}
+console.log("you have entered a correct number")
 
 //pro-4
 const mean =(a,b,c,d)=>{
