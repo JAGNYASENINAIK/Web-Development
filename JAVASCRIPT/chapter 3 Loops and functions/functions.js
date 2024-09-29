edb51b24ccc2b2e4a83052fcd0c362a82de14bf7
JAGNYASENI NAIK <reenunaik711@gmail.com>
2024-09-29 11:43:11 +0530

    created function.js

---
+++ JAVASCRIPT/chapter 3 Loops and functions/functions.js
@@ -0,0 +1,9 @@
+function onePlusAvg(x,y){
+    return 1+(x+y)/2
+}
+let a = 3
+let b = 7
+let c = 5
+console.log("one plus average of a and b is",onePlusAvg(a,b))
+console.log("one plus average of b and c is",onePlusAvg(b,c))
+console.log("one plus average of a and c is",onePlusAvg(a,c))
