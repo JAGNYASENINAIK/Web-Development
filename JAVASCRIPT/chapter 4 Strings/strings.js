1ddcf21090fa70636a66f3a221527c964862a2ca
JAGNYASENI NAIK <reenunaik711@gmail.com>
2024-09-30 07:00:46 +0530

    added

--- JAVASCRIPT/chapter 4 Strings/strings.js
+++ JAVASCRIPT/chapter 4 Strings/strings.js
@@ -1,3 +1,9 @@
 let name = "JAGNYASENI"
 console.log(name)
+console.log(name[1])
+console.log(name[6])
 console.log(name.length)
+let friend = 'kriti'
+console.log(friend)
+// let myfriend="reeen' //don't use  
+// console.log(myfriend)
