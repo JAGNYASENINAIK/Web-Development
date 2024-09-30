3978ffac6bcb0e100e1cf505faf6bc7d5dae7368
JAGNYASENI NAIK <reenunaik711@gmail.com>
2024-09-30 07:31:11 +0530

    added

--- JAVASCRIPT/chapter 4 Strings/string_methods.js
+++ JAVASCRIPT/chapter 4 Strings/string_methods.js
@@ -1,6 +1,9 @@
-let name ="ReeNu"
+let name ="ReeiinNu"
 console.log(name.length)
-let hame ="reeenu\""
+let hame ="reeenuuu\""
 console.log(hame.length)
 console.log(name.toUpperCase())
 console.log(name.toLowerCase())
+console.log(name.slice(3,6))
+console.log(hame.slice(3))
+console.log(name.replace("ii","jj"))
