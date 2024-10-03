67ca9e8fccc61c01389977108ca5f35b084f382a
JAGNYASENI NAIK <reenunaik711@gmail.com>
2024-10-03 19:21:14 +0530

    updated

--- JAVASCRIPT/chapter 4 Strings/string_methods.js
+++ JAVASCRIPT/chapter 4 Strings/string_methods.js
@@ -15,8 +15,8 @@ console.log(frnd2.trim())
 let  fr = "shivika " + " harry"
 let n =fr.length;
 console.log(fr[0])
-console.log(fr[1])
-console.log(fr[2])
+// console.log(fr[1])
+// console.log(fr[2])
 console.log(fr[3])
 console.log(fr[4])
 console.log(fr)
