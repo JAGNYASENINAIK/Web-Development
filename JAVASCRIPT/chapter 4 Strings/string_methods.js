8c80405538a7762f9b51cfe3a1d4fca65d1cc66a
JAGNYASENI NAIK <reenunaik711@gmail.com>
2024-10-03 19:21:56 +0530

    updated

--- JAVASCRIPT/chapter 4 Strings/string_methods.js
+++ JAVASCRIPT/chapter 4 Strings/string_methods.js
@@ -1,24 +1,24 @@
 let name ="ReeNu"
-console.log(name.length)
-let hame ="reeenuuu\""
-console.log(hame.length)
-console.log(name.toUpperCase())
-console.log(name.toLowerCase())
-console.log(name.slice(3,6))
-console.log(hame.slice(3))
-console.log(name.replace("ii","jj"))
-let frnd ="kiran"
-console.log(name.concat ( "  is a frnd of "),frnd ," okay")
-let frnd2 ="        varun             "
-console.log(frnd2)
-console.log(frnd2.trim())
+// console.log(name.length)
+// let hame ="reeenuuu\""
+// console.log(hame.length)
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.slice(3,6))
+// console.log(hame.slice(3))
+// console.log(name.replace("ii","jj"))
+// let frnd ="kiran"
+// console.log(name.concat ( "  is a frnd of "),frnd ," okay")
+// let frnd2 ="        varun             "
+// console.log(frnd2)
+// console.log(frnd2.trim())
 let  fr = "shivika " + " harry"
 let n =fr.length;
-console.log(fr[0])
+// console.log(fr[0])
 // console.log(fr[1])
 // console.log(fr[2])
-console.log(fr[3])
-console.log(fr[4])
+// console.log(fr[3])
+// console.log(fr[4])
 console.log(fr)
 
 // quiz
