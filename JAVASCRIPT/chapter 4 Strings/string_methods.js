78c8adcc56571e89ef015de73d2c4c99b663a799
JAGNYASENI NAIK <reenunaik711@gmail.com>
2024-10-03 19:20:29 +0530

    updated

--- JAVASCRIPT/chapter 4 Strings/string_methods.js
+++ JAVASCRIPT/chapter 4 Strings/string_methods.js
@@ -5,11 +5,11 @@ console.log(hame.length)
 console.log(name.toUpperCase())
 console.log(name.toLowerCase())
 console.log(name.slice(3,6))
-// console.log(hame.slice(3))
-// console.log(name.replace("ii","jj"))
-// let frnd ="kiran"
-// // console.log(name.concat ( "  is a frnd of "),frnd ," okay")
-// let frnd2 ="        varun             "
+console.log(hame.slice(3))
+console.log(name.replace("ii","jj"))
+let frnd ="kiran"
+// console.log(name.concat ( "  is a frnd of "),frnd ," okay")
+let frnd2 ="        varun             "
 console.log(frnd2)
 console.log(frnd2.trim())
 let  fr = "shivika " + " harry"
