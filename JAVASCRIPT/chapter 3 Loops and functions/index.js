e20c0d2b6919915757ec68ededd2b9f678cb43cc
JAGNYASENI NAIK <reenunaik711@gmail.com>
2024-10-04 21:39:49 +0530

    updated

--- JAVASCRIPT/chapter 3 Loops and functions/index.js
+++ JAVASCRIPT/chapter 3 Loops and functions/index.js
@@ -1,49 +1,49 @@
-// // //program to add first n natural numbers
-// // let sum=0
-// // const prompt=require("prompt-sync")({sigint:true}); 
-// // let n = prompt("enter the value of n :")
-// // n=Number.parseInt(n)
-// // for(let i =0 ;i<n;i++){
-// //     sum+=(i+1) // sum=sum+(i+1)
-// //     console.log(i+1) ,"+";
-// // }
-// // console.log("sum of first "   +n   + "-natural number is "  +    sum) 
-
+// //program to add first n natural numbers
 // let sum=0
 // const prompt=require("prompt-sync")({sigint:true}); 
 // let n = prompt("enter the value of n :")
 // n=Number.parseInt(n)
-// for(var i =0 ;i<n;i++){
+// for(let i =0 ;i<n;i++){
 //     sum+=(i+1) // sum=sum+(i+1)
 //     console.log(i+1) ,"+";
 // }
-// console.log("sum of first "   +n   + "- natural number is "  +    sum) 
+// console.log("sum of first "   +n   + "-natural number is "  +    sum) 
 
-// console.log(i)
+let sum=0
+const prompt=require("prompt-sync")({sigint:true}); 
+let n = prompt("enter the value of n :")
+n=Number.parseInt(n)
+for(var i =0 ;i<n;i++){
+    sum+=(i+1) // sum=sum+(i+1)
+    console.log(i+1) ,"+";
+}
+console.log("sum of first "   +n   + "- natural number is "  +    sum) 
 
-// // //factorial
+console.log(i)
 
-// // let fact=1
-// // const prompt=require("prompt-sync")({sigint:true}); 
-// // let n = prompt("enter the value of n :")
-// // n=Number.parseInt(n)
-// // for(let i =1 ;i<n;i++){
-// //     fact*=(i+1) 
-// //     //console.log(i*1) ,"+";
-// // }
-// // console.log("factorial of first "   +n   + "-natural number is "  +    fact) 
+// //factorial
 
-// // let obj ={
-// //     reenu:90,
-// //     sinu:89,
-// //     rahul:78,
-// //     jakul:60,
-// //     pritul:20,
-// // }
-// // for ( let a in obj){
-// //     console.log("Marks of  " +a+ " are" + - obj[a])
-// // }
+// let fact=1
+// const prompt=require("prompt-sync")({sigint:true}); 
+// let n = prompt("enter the value of n :")
+// n=Number.parseInt(n)
+// for(let i =1 ;i<n;i++){
+//     fact*=(i+1) 
+//     //console.log(i*1) ,"+";
+// }
+// console.log("factorial of first "   +n   + "-natural number is "  +    fact) 
 
-// // for (let b of "reenu"){
-// //     console.log(b)
-// // }
+// let obj ={
+//     reenu:90,
+//     sinu:89,
+//     rahul:78,
+//     jakul:60,
+//     pritul:20,
+// }
+// for ( let a in obj){
+//     console.log("Marks of  " +a+ " are" + - obj[a])
+// }
+
+// for (let b of "reenu"){
+//     console.log(b)
+// }
