35d9abde497394f714e24070ad0a056a6ec953af
Reenu <reenunaik711@gmail.com>
2025-12-02 19:25:50 +0530

    new added

---
+++ JAVASCRIPT/JS_APNA_CLG/2nd v/index.js
@@ -0,0 +1,6 @@
+let num=prompt("ENTER A NUMBER");
+if(num%5===0){
+    console.log( num ,"is multiple by 5")
+} else{
+    console.log( num ,"is not multiple by 5")
+}
---
+++ JAVASCRIPT/JS_APNA_CLG/2nd v/marks.js
@@ -0,0 +1,16 @@
+// let Grade;
+let marks = 89;
+
+if (marks <= 100 && marks >= 90) {
+    console.log("A");
+} else if (marks <= 89 && marks >= 70) {
+    console.log("B");
+} else if (marks <= 69 && marks >= 60) {
+    console.log("C");
+} else if (marks <= 59 && marks >= 50) {
+    console.log("D");
+} else if (marks <= 49 && marks >= 0) {
+    console.log("F");
+} else {
+    console.log("Marks is not valid");
+}
