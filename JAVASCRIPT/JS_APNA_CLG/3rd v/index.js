69a6dd816365f119380ed162b3531c953815e5c6
Reenu <reenunaik711@gmail.com>
2025-12-02 21:23:28 +0530

    Update index.js

--- JAVASCRIPT/JS_APNA_CLG/3rd v/index.js
+++ JAVASCRIPT/JS_APNA_CLG/3rd v/index.js
@@ -1,4 +1,26 @@
+// 1
 // let reenu;
 // for( let i=0;i<=8;i++){
 //     console.log("reenu")
 // }
+//2
+// for(let i=0;i<=100;i++){
+    
+// if(i%2===0){
+
+// console.log("these are the even numbers",i)}}
+// 3
+// let game=prompt("Enter a number between 0-100");
+// console.log(game)
+// let num=68;
+// while(game!=num){
+// game=prompt("You entered the wrong number")
+// }
+// console.log("Congratulations !!!!! You Won")
+
+// 4
+let userPr=prompt("Enter your full name");
+let l=userPr.length;
+let username=`@${userPr}${l}`
+console.log(username)
+
