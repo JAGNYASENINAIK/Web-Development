f46c4cebd490131277e406c362ddccd26a3d2f48
Reenu <reenunaik711@gmail.com>
2025-12-05 20:03:37 +0530

    added new

---
+++ JAVASCRIPT/JS_APNA_CLG/5th v/js.js
@@ -0,0 +1,27 @@
+// function countVowels(str) {
+//     let count=0
+//     for(const char of str){
+//         if(char ==="a" ||char ==="e" ||char ==="i" ||char ==="o" ||char ==="u"  ){
+//             count++;
+//         }
+//     }
+//      return count;
+// }
+
+
+// const countVow=(str)=>{
+//     let count=0
+//     for(const char of str){
+//         if(char ==="a" ||char ==="e" ||char ==="i" ||char ==="o" ||char ==="u"  ){
+//             count++;
+//         }
+//     }
+//      return count;
+// }
+
+let arr=[2,3,4,5,6,7]
+arr.forEach((val)=>{
+    console.log(val*val);
+}
+
+)
